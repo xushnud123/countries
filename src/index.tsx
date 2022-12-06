bfb14f94d4aefc85c39b6533e28a0e6512fc9445
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2022-12-06 16:35:50 +0500

    skleten add project

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { QueryClient, QueryClientProvider } from "react-query";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
--- src/pages/landing/countries/countries.tsx
+++ src/pages/landing/countries/countries.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
-import { Loader } from "components";
+import Skeleton from "react-loading-skeleton";
+import { temporaryData } from "constant/countries";
 
 import useRestApi from "modules/countries/hooks/useCountries";
 
@@ -9,29 +10,40 @@ interface CountriesProps {}
 
 const Countries: FC<CountriesProps> = () => {
   const { data, isLoading } = useRestApi();
-  
+
   if (isLoading) {
-    return <Loader />;
+    return (
+      <div className={cls.wrapper}>
+        <div className={cls.allCard}>
+          {temporaryData?.map((item: number) => (
+            <div className={cls.cards} key={item}>
+              <Skeleton className={cls.card} width='100%' height='100%' />
+              <Skeleton className={cls.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className={cls.wrapper}>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className={cls.allCard}>
-          {data?.map((item: any, index: number) => (
-            <div className={cls.cards} key={index}>
+      <div className={cls.allCard}>
+        {data?.map((item: any, index: number) => (
+          <div className={cls.cards} key={index}>
+            <div className={cls.card}>
               <img
                 className={cls.card}
                 src={item?.flags?.svg}
                 alt='img not found'
               />
-              <h5>{item.altSpellings[1] || item.altSpellings[0]}</h5>
             </div>
-          ))}
-        </div>
-      )}
+            <h5 className={cls.title}>
+              {item.altSpellings[1] || item.altSpellings[0]}
+            </h5>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
