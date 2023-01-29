db30a7e3a6876024e37a7723a11040407780b696
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2023-01-29 23:52:30 +0500

    feat: hero update

--- src/pages/landing/components/hero/hero.tsx
+++ src/pages/landing/components/hero/hero.tsx
@@ -1,4 +1,12 @@
 import { FC, useEffect, useRef } from "react";
+import cx from "classnames";
+
+import population from "assets/images/hero/population.webp";
+import currency from "assets/images/hero/currency.webp";
+import language from "assets/images/hero/language.webp";
+import region from "assets/images/hero/region.webp";
+import capital from "assets/images/hero/capital.webp";
+import countries from 'assets/images/hero/countries.webp'
 
 import cls from "./hero.module.scss";
 
@@ -42,61 +50,60 @@ const Hero: FC<HeroProps> = () => {
     <div className={cls.wrapper}>
       <h1 className={cls.title}>
         Search for information about <span>Countries</span> with us{" "}
-        <span>fast</span>,
-        <span>convenient</span>, <span>reliable</span>.
+        <span>fast</span>,<span>convenient</span>, <span>reliable</span>.
       </h1>
       <div ref={ref} onScroll={onScroll} className={cls.container}>
         <div className={cls.row}>
           <div ref={card1} className={cls["left-cart-1"]}>
-            <div className={cls["card-1"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-1"], cls.option)}>
+              <img src={population} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Population</h1>
+              </div>
             </div>
           </div>
           <div ref={card2} className={cls["left-cart-2"]}>
-            <div className={cls["card-1"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-1"], cls.option)}>
+              <img src={region} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Sub-Region</h1>
+              </div>
             </div>
-            <div className={cls["card-2"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-2"], cls.option)}>
+              <img src={region} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Region</h1>
+              </div>
             </div>
           </div>
           <div ref={card3} className={cls["center"]}>
-            <div className={cls["card-1"]}>
+            <div className={cx(cls["card-1"], cls.option)}>
               <img
-                src='https://source.unsplash.com/1000x1000/?girls'
+                src={countries}
                 alt='img not found'
               />
             </div>
           </div>
           <div ref={card4} className={cls["right-cart-2"]}>
-            <div className={cls["card-2"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-2"], cls.option)}>
+              <img src={capital} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Capital</h1>
+              </div>
             </div>
-            <div className={cls["card-1"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-1"], cls.option)}>
+              <img src={language} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Language</h1>
+              </div>
             </div>
           </div>
           <div ref={card5} className={cls["left-cart-1"]}>
-            <div className={cls["card-1"]}>
-              <img
-                src='https://source.unsplash.com/collection/190727/500x500'
-                alt='img not found'
-              />
+            <div className={cx(cls["card-1"], cls.option)}>
+              <img src={currency} alt='img not found' />
+              <div className={cls.info}>
+                <h1>Currency</h1>
+              </div>
             </div>
           </div>
         </div>
